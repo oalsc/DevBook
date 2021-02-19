20b02a27f59c7a080f9e903fc3081677d2c9c243
oalsc <andersonluissalazar@outlook.com>
2021-02-18 23:01:48 -0300

    Hash

--- api/go.mod
+++ api/go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/gorilla/mux v1.8.0
 	github.com/joho/godotenv v1.3.0
+	golang.org/x/crypto v0.0.0-20210218145215-b8e89b74b9df
 )
---
+++ api/src/seguranca/seguranca.go
@@ -0,0 +1,13 @@
+package seguranca
+
+import "golang.org/x/crypto/bcrypt"
+
+// Hash recebe uma string e coloca um hash nela
+func Hash(senha string) ([]byte, error) {
+	return bcrypt.GenerateFromPassword([]byte(senha), bcrypt.DefaultCost)
+}
+
+// VerificarSenha verifica a senha com um hash e retorna se elas sao iguais
+func VerificarSenha(senhaComHash, senhaString string) error {
+	return bcrypt.CompareHashAndPassword([]byte(senhaComHash), []byte(senhaString))
+}
